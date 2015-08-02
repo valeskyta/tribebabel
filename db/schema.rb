3d9f9a6e040ddf2de8db85c1e645acb9fe9f484e
Valeskyta <valeska.arquitecto@gmail.com>
2015-08-02 01:30:35 -0400

    add address to events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150712152318) do
+ActiveRecord::Schema.define(version: 20150802052308) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20150712152318) do
     t.integer  "visibility"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "address"
   end
 
   create_table "invitation_lists", force: :cascade do |t|
