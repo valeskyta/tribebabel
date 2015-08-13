cfc89a39e9b779c820593ec3e39a599d71ec9288
Valeskyta <valeska.arquitecto@gmail.com>
2015-08-13 16:02:34 -0400

    thumbs and default images

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -19,7 +19,7 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
-  ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+  ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.gif"].compact.join('_'))
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   end
@@ -36,6 +36,12 @@ class PictureUploader < CarrierWave::Uploader::Base
      process :resize_to_fit => [64, 64]
    end
 
+     version :thumb_profile do
+     process :resize_to_fit => [220, 220]
+   end
+     version :thumb_members do
+     process :resize_to_fit => [100, 100]
+   end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
